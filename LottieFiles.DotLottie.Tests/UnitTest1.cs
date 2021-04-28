bab4e80c85a5576da17f70957e07d4deb058aae7
Alex Harper <alex@lottiefiles.com>
2021-04-28 18:26:06 +0100

    feat: added some basic tests

--- LottieFiles.DotLottie.Tests/UnitTest1.cs
+++ LottieFiles.DotLottie.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -6,6 +7,10 @@ namespace LottieFiles.IO.Tests
 {
     public class Tests
     {
+        Uri sampleDotLottie = new Uri("https://dotlottie.io/sample_files/animation.lottie");
+        Uri sampleFakeDotLottie = new Uri("https://dotlottie.io/sample_files/animation.shottie");
+
+
         [SetUp]
         public void Setup()
         {
@@ -13,14 +18,50 @@ namespace LottieFiles.IO.Tests
         }
 
         [Test]
-        public async Task Test1()
+        public async Task Open_File_Async()
         {
             //Try parse a lottie
             var stream = File.OpenRead("Lotties/animation-external-image.lottie");
             DotLottie lottie = await DotLottie.OpenAsync(stream);
 
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task Open_Uri_Async()
+        {
+            //Try parse a lottie            
+            DotLottie lottie = await DotLottie.OpenAsync(sampleDotLottie);
 
             Assert.Pass();
+        }
+
+        [Test]
+        public async Task Open_Uri_Async_Fail_Bad_Uri()
+        {
+            Assert.CatchAsync(async () =>
+            {
+                DotLottie lottie = await DotLottie.OpenAsync(sampleFakeDotLottie);
+            });            
+        }
+
+        [Test]
+        public async Task Temp_Open_Dotlottie_And_Save()
+        {
+            var stream = File.OpenRead("Lotties/animation-external-image.lottie");
+            var ms = new MemoryStream();
+            stream.CopyTo(ms);
+
+            DotLottie lottie = await DotLottie.OpenAsync(stream);
+            var existingData = ms.GetBuffer();
+            var exportedData = lottie.GetData();
+            File.WriteAllBytes("temp.lottie", exportedData);
+        }
+
+        [Test]
+        public void Fail_To_Create_Manifest()
+        {
+            
         }
     }
 }
